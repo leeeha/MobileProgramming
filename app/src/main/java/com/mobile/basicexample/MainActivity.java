8c5f74578ee77172db5b37867f1f4d0a83997c82
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-16 02:41:01 +0900

    WebView 오류 해결하기

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -1,42 +1,69 @@
 package com.mobile.basicexample;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
-import android.widget.MultiAutoCompleteTextView;
-import android.widget.ViewFlipper;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+// 앱이 동작하지 않는다. 저번엔 괜찮았는데 뭐가 문제일까...
 public class MainActivity extends AppCompatActivity {
+    EditText edtUrl;
+    Button btnGo, btnBack;
+    WebView web;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Button btnStart, btnEnd;
-        final ViewFlipper viewFlipper;
+        edtUrl = findViewById(R.id.edtUrl);
+        btnGo = findViewById(R.id.btnGo);
+        btnBack = findViewById(R.id.btnBack);
+        web = findViewById(R.id.web);
 
-        btnStart = findViewById(R.id.btnStart);
-        btnEnd = findViewById(R.id.btnEnd);
-        viewFlipper = findViewById(R.id.viewFlipper1);
+        web.setWebViewClient(new MyWebViewClient());
 
-        btnStart.setOnClickListener(new View.OnClickListener() {
+        WebSettings webSet = web.getSettings();
+        webSet.setBuiltInZoomControls(true);
+
+        // 이동 버튼을 클릭하면, 사용자가 입력한 URL을 웹뷰에 로딩한다.
+        btnGo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                viewFlipper.startFlipping();
-                viewFlipper.setFlipInterval(1000);
+                web.loadUrl(edtUrl.getText().toString());
             }
         });
 
-        btnEnd.setOnClickListener(new View.OnClickListener() {
+        // 이전 버튼 누르면, 웹에서 이전 화면으로 돌아간다.
+        btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                viewFlipper.stopFlipping();
+                web.goBack();
+            }
+        });
+
+        edtUrl.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(keyCode == event.KEYCODE_ENTER){
+                    btnGo.callOnClick();
+                }
+                return false;
             }
         });
     }
+
+    // inner class
+    class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+    }
 }
+
