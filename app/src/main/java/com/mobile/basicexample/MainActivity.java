ee34e95be343a0a25e5ef944ee1f62f285262737
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-15 23:51:49 +0900

    AutoCompleteTextView, MultiAutoCompleteTextView

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -1,120 +1,30 @@
 package com.mobile.basicexample;
 
-import androidx.annotation.NonNull;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.SystemClock;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.Chronometer;
-import android.widget.DatePicker;
-import android.widget.RadioButton;
-import android.widget.TextView;
-import android.widget.TimePicker;
-
 public class MainActivity extends AppCompatActivity {
-    Chronometer chrono;
-    RadioButton rdoCal, rdoTime;
-    DatePicker datePicker;
-    TimePicker timePicker;
-    TextView tvYear, tvMonth, tvDay, tvHour, tvMinute;
-    int selectYear, selectMonth, selectDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("날짜와 시간 예약하기 (수정)");
 
-        chrono = findViewById(R.id.chronometer1);
-        rdoCal = findViewById(R.id.rdoCal);
-        rdoTime = findViewById(R.id.rdoTime);
+        String[] items = { "CSI-뉴욕", "CSI-라스베가스", "CSI-마이애미", "Friends",
+                            "Fringe", "Lost"};
 
-        datePicker = findViewById(R.id.datePicker1);
-        timePicker = findViewById(R.id.timePiker1);
+        AutoCompleteTextView auto = findViewById(R.id.autoCompleteTextView1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line, items);
+        auto.setAdapter(adapter);
 
-        tvYear = findViewById(R.id.tvYear);
-        tvMonth = findViewById(R.id.tvMonth);
-        tvDay = findViewById(R.id.tvDay);
-        tvHour = findViewById(R.id.tvHour);
-        tvMinute = findViewById(R.id.tvMinute);
-
-        rdoCal.setVisibility(View.INVISIBLE);
-        rdoTime.setVisibility(View.INVISIBLE);
-        datePicker.setVisibility(View.INVISIBLE);
-        timePicker.setVisibility(View.INVISIBLE);
-
-        // 크로노미터를 누르면 시간 측정 시작 && 라디오 버튼 보이게 하기
-        chrono.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                chrono.setBase(SystemClock.elapsedRealtime());
-                chrono.start();
-                chrono.setTextColor(Color.RED);
-
-                rdoCal.setVisibility(View.VISIBLE);
-                rdoTime.setVisibility(View.VISIBLE);
-            }
-        });
-
-        // 날짜 설정 라디오 버튼을 누르면, datePicker 보이게 하기
-        rdoCal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                timePicker.setVisibility(View.INVISIBLE);
-                datePicker.setVisibility(View.VISIBLE);
-            }
-        });
-
-        // 시간 설정 라디오 버튼을 누르면, timePicker 보이게 하기
-        rdoTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                timePicker.setVisibility(View.VISIBLE);
-                datePicker.setVisibility(View.INVISIBLE);
-            }
-        });
-
-        // 오레오 버전 이상에서만 정상 작동
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            datePicker.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
-                @Override
-                public void onDateChanged(DatePicker datePicker, int year, int month, int dayOfMonth) {
-                    selectYear = year;
-                    selectMonth = month + 1;
-                    selectDay = dayOfMonth;
-                }
-            });
-        }
-
-        // tvYear를 길게 클릭하면 크로노미터 종료
-        // 라디오 그룹, 데이트피커, 타임피커 안 보이게 하기
-        tvYear.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                chrono.stop();
-                chrono.setTextColor(Color.BLUE);
-
-                tvYear.setText(Integer.toString(selectYear));
-                tvMonth.setText(Integer.toString(selectMonth));
-                tvDay.setText(Integer.toString(selectDay));
-
-                tvHour.setText(Integer.toString(timePicker.getCurrentHour()));
-                tvMinute.setText(Integer.toString(timePicker.getCurrentMinute()));
-
-                rdoCal.setVisibility(View.INVISIBLE);
-                rdoTime.setVisibility(View.INVISIBLE);
-                datePicker.setVisibility(View.INVISIBLE);
-                timePicker.setVisibility(View.INVISIBLE);
-
-                return false;
-            }
-        });
+        MultiAutoCompleteTextView multi = findViewById(R.id.multiAutoCompleteTextView1);
+        MultiAutoCompleteTextView.CommaTokenizer token = new MultiAutoCompleteTextView.CommaTokenizer();
+        multi.setTokenizer(token);
+        multi.setAdapter(adapter);
     }
-
-
 }
