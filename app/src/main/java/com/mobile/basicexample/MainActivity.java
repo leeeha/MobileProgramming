3a078890f3dc29b0c7e01cbfed150517b7fae551
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-19 00:40:13 +0900

    옵션 메뉴에 따라 이미지 변경하기

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -1,73 +1,59 @@
 package com.mobile.basicexample;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.SubMenu;
-import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    LinearLayout baseLayout;
-    Button button1;
+    RelativeLayout baseLayout;
+    EditText editText;
+    ImageView imageView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("옵션 메뉴로 배경색 바꾸기");
+        setTitle("옵션 메뉴에 따라 이미지 바꾸기");
 
         baseLayout = findViewById(R.id.baseLayout);
-        button1 = findViewById(R.id.button1);
+        editText = findViewById(R.id.editText);
+        imageView = findViewById(R.id.imageView);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
 
-        // 방법1: 인플레이터를 통해 menu1.xml 파일을 메모리에 객체화 시킨다.
-        //MenuInflater mInflater = getMenuInflater();
-        //mInflater.inflate(R.menu.menu1, menu);
-
-        // 방법2: 직접 Menu.add() 메소드로 메뉴 항목 추가하기
-        menu.add(0, 1, 0, "배경색 (빨강)");
-        menu.add(0, 2, 0, "배경색 (초록)");
-        menu.add(0, 3, 0, "배경색 (파랑)");
-
-        SubMenu sMenu = menu.addSubMenu("버튼 변경 >> ");
-        sMenu.add(0, 4, 0, "버튼 45도 회전");
-        sMenu.add(0, 5, 0, "버튼 2배 확대");
+        // 인플레이터로 menu1.xml 파일을 메모리에 객체화 시킨다.
+        MenuInflater mInflater = getMenuInflater();
+        mInflater.inflate(R.menu.menu1, menu);
 
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
         switch(item.getItemId()){
-            //case R.id.itemRed:
-            case 1:
-                baseLayout.setBackgroundColor(Color.RED);
+            case R.id.rotateMenu:
+                String angle = editText.getText().toString();
+                imageView.setRotation(Integer.parseInt(angle));
                 return true;
-            //case R.id.itemGreen:
-            case 2:
-                baseLayout.setBackgroundColor(Color.GREEN);
+            case R.id.item1:
+                imageView.setImageResource(R.drawable.api70);
                 return true;
-            //case R.id.itemBlue:
-            case 3:
-                baseLayout.setBackgroundColor(Color.BLUE);
+            case R.id.item2:
+                imageView.setImageResource(R.drawable.api80);
                 return true;
-            //case R.id.subRotate:
-            case 4:
-                button1.setRotation(45); // 45도 회전
-                return true;
-            //case R.id.subSize:
-            case 5:
-                button1.setScaleX(2); // 가로로 2배 확대
+            case R.id.item3:
+                imageView.setImageResource(R.drawable.api90);
                 return true;
         }
 
