8838f3779c2a9cbb6cff02a3ef6516b0335df753
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-19 17:21:34 +0900

    파일 입출력으로 간단 일기장 만들기

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -4,72 +4,101 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOError;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    DatePicker dp;
+    EditText edtDiary;
+    Button btnWrite;
+    String fileName;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("파일 입출력");
+        setTitle("간단 일기장");
 
-        Button btnRead, btnWrite;
-        btnRead = findViewById(R.id.btnRead);
+        dp = findViewById(R.id.datePicker1);
+        edtDiary = findViewById(R.id.edtDiary);
         btnWrite = findViewById(R.id.btnWrite);
 
-        // 파일에 쓰기
+        // Calendar 클래스를 이용해 현재 날짜(연, 월, 일)로 DatePicker 초기화
+        Calendar cal = Calendar.getInstance();
+        int cYear = cal.get(Calendar.YEAR);
+        int cMonth = cal.get(Calendar.MONTH);
+        int cDay = cal.get(Calendar.DAY_OF_MONTH);
+
+        dp.init(cYear, cMonth, cDay, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                // DatePicker에서 선택한 날짜에 따라 파일명을 "연_월_일.txt"로 설정
+                fileName = year + "_" + (monthOfYear + 1) + "_"  + dayOfMonth + ".txt";
+
+                // 해당하는 파일을 찾아서 EditText에 보여주기
+                String str = readDiary(fileName);
+                edtDiary.setText(str);
+
+                btnWrite.setEnabled(true);
+            }
+        });
+
+        // 버튼을 클릭하면, EditText에 입력한 내용을 파일에 저장
         btnWrite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try {
-                    FileOutputStream outFs = openFileOutput("file.txt",
-                            Context.MODE_APPEND);
-
-                    // file.txt 파일에 문자열 작성 후 저장
-                    String str = "쿡북 안드로이드";
+                    FileOutputStream outFs = openFileOutput(fileName, Context.MODE_PRIVATE);
+                    String str = edtDiary.getText().toString();
                     outFs.write(str.getBytes(StandardCharsets.UTF_8));
                     outFs.close();
 
-                    Toast.makeText(getApplicationContext(), "file.txt가 생성됨", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), fileName + "이 저장됨",
+                            Toast.LENGTH_SHORT).show();
+
                 } catch (IOException e) {
                     e.printStackTrace();
-                }
-            }
-        });
-
-        // 파일 읽기
-        btnRead.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    FileInputStream inFs = openFileInput("file.txt");
-                    byte[] txt = new byte[30];
-
-                    // byte 타입으로 파일 내용 읽어오기
-                    inFs.read(txt);
-
-                    // byte 타입의 인자를 받아서 String 객체 생성
-                    String str = new String(txt);
-
-                    // 파일에서 읽어온 문자열을 토스트 메시지에 띄우기
-                    Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT).show();
-
-                    inFs.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    Toast.makeText(getApplicationContext(), "읽을 파일이 없음", Toast.LENGTH_SHORT).show();
                 }
 
             }
         });
     }
+
+    private String readDiary(String fileName) {
+        String diaryStr = null;
+        FileInputStream inFs;
+
+        try{
+            // 파일을 읽어서 byte 타입의 배열에 저장하기
+            inFs = openFileInput(fileName);
+            byte[] txt = new byte[500];
+            inFs.read(txt);
+            inFs.close();
+
+            // byte 타입의 배열로 String 객체 생성 후 공백 제거
+            diaryStr = (new String(txt)).trim();
+
+            btnWrite.setText("수정하기");
+
+        } catch (IOException e){
+            edtDiary.setHint("일기 없음");
+            btnWrite.setText("새로 저장");
+        }
+
+        return diaryStr;
+    }
+
 }
 
