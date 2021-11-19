eba0566113bf246e0c2cf2ab71e78053c13b6527
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-19 16:15:38 +0900

    파일 입출력 기본 예제

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -1,103 +1,73 @@
 package com.mobile.basicexample;
 
-import android.content.DialogInterface;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.Display;
-import android.view.Gravity;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-/*
-    1. 버튼을 클릭하면, 메인 화면에서 입력한 텍스트가 대화상자에 뜨도록!
-    2. 대화 상자에 확인 버튼을 클릭하면, 대화상자에서 입력한 텍스트가 메인 화면에 뜨도록!
-    3. 취소를 클릭하면 토스트 메시지가 메인 화면의 임의적인 위치에 뜨도록!
- */
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
-    EditText mainEdtName, mainEdtEmail;
-    Button button1;
-    EditText dlgEdtName, dlgEdtEmail;
-    TextView toastText1;
-    View dialogView, toastView; // XML 인플레이트의 결과를 저장하는 변수
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("사용자 정보 입력 (수정)");
+        setTitle("파일 입출력");
 
-        mainEdtName = findViewById(R.id.mainEdtName);
-        mainEdtEmail = findViewById(R.id.mainEdtEmail);
-        button1 = findViewById(R.id.button1);
+        Button btnRead, btnWrite;
+        btnRead = findViewById(R.id.btnRead);
+        btnWrite = findViewById(R.id.btnWrite);
 
-        // 버튼을 클릭하면, 메인 화면에서 입력한 텍스트가 대화상자에 뜨도록
-        button1.setOnClickListener(new View.OnClickListener() {
+        // 파일에 쓰기
+        btnWrite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // dialog1.xml 파일을 dialogView 변수에 인플레이트 (메모리에 객체화) 시킨다.
-                dialogView = View.inflate(MainActivity.this, R.layout.dialog1, null);
+                try {
+                    FileOutputStream outFs = openFileOutput("file.txt",
+                            Context.MODE_APPEND);
 
-                // 메인 액티비티에 대화상자 빌더 생성
-                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                    // file.txt 파일에 문자열 작성 후 저장
+                    String str = "쿡북 안드로이드";
+                    outFs.write(str.getBytes(StandardCharsets.UTF_8));
+                    outFs.close();
 
-                // 대화상자의 제목, 아이콘, 뷰 설정
-                dlg.setTitle("사용자 정보 입력");
-                dlg.setIcon(R.drawable.ic_launcher_foreground);
-                dlg.setView(dialogView);
+                    Toast.makeText(getApplicationContext(), "file.txt가 생성됨", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
-                // 대화상자의 EditText 객체 생성
-                dlgEdtName = dialogView.findViewById(R.id.dlgEdt1);
-                dlgEdtEmail = dialogView.findViewById(R.id.dlgEdt2);
+        // 파일 읽기
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    FileInputStream inFs = openFileInput("file.txt");
+                    byte[] txt = new byte[30];
 
-                // 메인 화면에서 입력한 텍스트가 대화상자에 뜨도록
-                dlgEdtName.setText(mainEdtName.getText().toString());
-                dlgEdtEmail.setText(mainEdtEmail.getText().toString());
+                    // byte 타입으로 파일 내용 읽어오기
+                    inFs.read(txt);
 
-                // 확인 버튼을 누르면, 대화상자에서 입력한 텍스트가 메인 화면에 뜨도록
-                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        mainEdtName.setText(dlgEdtName.getText().toString());
-                        mainEdtEmail.setText(dlgEdtEmail.getText().toString());
-                    }
-                });
+                    // byte 타입의 인자를 받아서 String 객체 생성
+                    String str = new String(txt);
 
-                // 취소 버튼을 누르면, 토스트 메시지가 임의적인 위치에 뜨도록
-                dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast toast = new Toast(MainActivity.this);
+                    // 파일에서 읽어온 문자열을 토스트 메시지에 띄우기
+                    Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT).show();
 
-                        // toast1.xml 파일을 toastView 변수에 인플레이트 (메모리에 객체화) 시킨다.
-                        toastView = View.inflate(MainActivity.this, R.layout.toast1, null);
+                    inFs.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "읽을 파일이 없음", Toast.LENGTH_SHORT).show();
+                }
 
-                        // 토스트 메시지의 텍스트 뷰 변경하기
-                        toastText1 = toastView.findViewById(R.id.toastText1);
-                        toastText1.setText("취소했습니다");
-
-                        // 인플레이트 시킨 toast1.xml로 뷰 설정
-                        toast.setView(toastView);
-                        toast.setDuration(Toast.LENGTH_SHORT);
-
-                        // 임의적인 위치에 토스트 메시지 띄우기
-                        Display display = ((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
-                        int xOffset = (int) (Math.random() * display.getWidth());
-                        int yOffset = (int) (Math.random() * display.getHeight());
-                        toast.setGravity(Gravity.TOP | Gravity.LEFT, xOffset, yOffset);
-
-                        toast.show();
-                    }
-                });
-
-                // 다이얼로그 띄우기
-                dlg.show();
             }
         });
     }
