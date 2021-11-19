7e24f3424ce8d9df690d9d1124b92d9b118d60f7
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-19 13:53:18 +0900

    대화 상자 띄우기 기본 예제

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mobile.basicexample;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.Gravity;
@@ -8,33 +9,34 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("토스트 메시지 연습");
+        setTitle("대화 상자 띄우기");
 
         final Button button1 = findViewById(R.id.button1);
-
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast tMsg = Toast.makeText(MainActivity.this,
-                        "토스트 연습", Toast.LENGTH_SHORT);
-
-                // getWidth, getHeight가 deprecated 되어서 토스트 메시지 위치가 랜덤으로 바뀌지 않는다,,,
-                Display display = ((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
-                int xOffset = (int) (Math.random() * display.getWidth());
-                int yOffset = (int) (Math.random() * display.getHeight());
-
-                tMsg.setGravity(Gravity.TOP | Gravity.LEFT, xOffset, yOffset);
-                tMsg.show();
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                dlg.setTitle("제목입니다");
+                dlg.setMessage("이곳이 내용입니다");
+                dlg.setIcon(R.mipmap.ic_launcher);
+                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "확인을 눌렀네요", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dlg.show();
             }
         });
-
     }
 }
 
