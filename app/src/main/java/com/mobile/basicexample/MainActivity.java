da419a3a3b2ac5127937d7a01eb4a06a611bfe21
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-19 15:22:48 +0900

    대화 상자에 입력한 내용을 메인 액티비티의 텍스트 뷰에서 보여주기

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -7,49 +7,66 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvName, tvEmail;
+    Button button1;
+    EditText dlgEdtName, dlgEdtEmail;
+    TextView toastText1;
+    View dialogView, toastView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("대화 상자 띄우기");
+        setTitle("사용자 정보 입력");
 
-        final Button button1 = findViewById(R.id.button1);
+        tvName = findViewById(R.id.tvName);
+        tvEmail = findViewById(R.id.tvEmail);
+        button1 = findViewById(R.id.button1);
+
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final String[] versionArray = new String[] {"마시멜로", "누가", "오레오"};
-                final boolean[] checkArray = new boolean[] {false, false, false};
+                dialogView = View.inflate(MainActivity.this, R.layout.dialog1, null);
 
                 AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
-                dlg.setTitle("좋아하는 버전은?");
-                dlg.setIcon(R.mipmap.ic_launcher);
 
-                // 라디오 버튼으로 변경
-//                dlg.setSingleChoiceItems(versionArray, 0,
-//                        new DialogInterface.OnClickListener() {
-//                            @Override
-//                            public void onClick(DialogInterface dialogInterface, int i) {
-//                                button1.setText(versionArray[i]);
-//                            }
-//                        });
+                dlg.setTitle("사용자 정보 입력");
+                dlg.setIcon(R.drawable.ic_launcher_foreground);
+                dlg.setView(dialogView);
 
-                // 체크박스로 변경
-                dlg.setMultiChoiceItems(versionArray, checkArray,
-                        new DialogInterface.OnMultiChoiceClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i, boolean b) {
-                                button1.setText(versionArray[i]);
-                            }
-                        });
+                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dlgEdtName = dialogView.findViewById(R.id.dlgEdt1);
+                        dlgEdtEmail = dialogView.findViewById(R.id.dlgEdt2);
 
-                dlg.setPositiveButton("닫기", null);
+                        tvName.setText(dlgEdtName.getText().toString());
+                        tvEmail.setText(dlgEdtEmail.getText().toString());
+                    }
+                });
+
+                dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast toast = new Toast(MainActivity.this);
+
+                        toastView = View.inflate(MainActivity.this, R.layout.toast1, null);
+                        toastText1 = toastView.findViewById(R.id.toastText1);
+                        toastText1.setText("취소했습니다");
+
+                        toast.setView(toastView);
+                        toast.show();
+                    }
+                });
+
                 dlg.show();
             }
         });
