526b459989d2b27fafc685ff7badad28879c9e86
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-19 13:57:04 +0900

    대화 상자에서 선택한 항목에 따라 버튼 텍스트 변경하기

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -24,16 +24,21 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final String[] versionArray = new String[] {"마시멜로", "누가", "오레오"};
+
                 AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
-                dlg.setTitle("제목입니다");
-                dlg.setMessage("이곳이 내용입니다");
+                dlg.setTitle("좋아하는 버전은?");
                 dlg.setIcon(R.mipmap.ic_launcher);
-                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(MainActivity.this, "확인을 눌렀네요", Toast.LENGTH_SHORT).show();
-                    }
-                });
+
+                dlg.setItems(versionArray,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                button1.setText(versionArray[i]);
+                            }
+                        });
+
+                dlg.setPositiveButton("닫기", null);
                 dlg.show();
             }
         });
