289f9e62087b1834fbae942118da8bf3af8779e5
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-19 18:26:44 +0900

    raw 폴더의 파일 읽기

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -16,88 +16,38 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOError;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-    DatePicker dp;
-    EditText edtDiary;
-    Button btnWrite;
-    String fileName;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("간단 일기장");
+        setTitle("raw 폴더 파일 처리");
 
-        dp = findViewById(R.id.datePicker1);
-        edtDiary = findViewById(R.id.edtDiary);
-        btnWrite = findViewById(R.id.btnWrite);
+        Button btnRead = findViewById(R.id.btnRead);
+        final EditText edtRaw = findViewById(R.id.edtRaw);
 
-        // Calendar 클래스를 이용해 현재 날짜(연, 월, 일)로 DatePicker 초기화
-        Calendar cal = Calendar.getInstance();
-        int cYear = cal.get(Calendar.YEAR);
-        int cMonth = cal.get(Calendar.MONTH);
-        int cDay = cal.get(Calendar.DAY_OF_MONTH);
-
-        dp.init(cYear, cMonth, cDay, new DatePicker.OnDateChangedListener() {
-            @Override
-            public void onDateChanged(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
-                // DatePicker에서 선택한 날짜에 따라 파일명을 "연_월_일.txt"로 설정
-                fileName = year + "_" + (monthOfYear + 1) + "_"  + dayOfMonth + ".txt";
-
-                // 해당하는 파일을 찾아서 EditText에 보여주기
-                String str = readDiary(fileName);
-                edtDiary.setText(str);
-
-                btnWrite.setEnabled(true);
-            }
-        });
-
-        // 버튼을 클릭하면, EditText에 입력한 내용을 파일에 저장
-        btnWrite.setOnClickListener(new View.OnClickListener() {
+        btnRead.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // raw_test.txt 파일 읽어서 에디트텍스트에 보여주기
+                InputStream inputS = getResources().openRawResource(R.raw.raw_test);
                 try {
-                    FileOutputStream outFs = openFileOutput(fileName, Context.MODE_PRIVATE);
-                    String str = edtDiary.getText().toString();
-                    outFs.write(str.getBytes(StandardCharsets.UTF_8));
-                    outFs.close();
-
-                    Toast.makeText(getApplicationContext(), fileName + "이 저장됨",
-                            Toast.LENGTH_SHORT).show();
-
+                    byte[] txt = new byte[inputS.available()];
+                    inputS.read(txt);
+                    edtRaw.setText(new String(txt));
+                    inputS.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
 
             }
         });
-    }
 
-    private String readDiary(String fileName) {
-        String diaryStr = null;
-        FileInputStream inFs;
-
-        try{
-            // 파일을 읽어서 byte 타입의 배열에 저장하기
-            inFs = openFileInput(fileName);
-            byte[] txt = new byte[500];
-            inFs.read(txt);
-            inFs.close();
-
-            // byte 타입의 배열로 String 객체 생성 후 공백 제거
-            diaryStr = (new String(txt)).trim();
-
-            btnWrite.setText("수정하기");
-
-        } catch (IOException e){
-            edtDiary.setHint("일기 없음");
-            btnWrite.setText("새로 저장");
-        }
-
-        return diaryStr;
     }
 
 }
