26e29df5d69a59cdd7ebda8beec2b78cf4d2ba1e
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-19 21:20:25 +0900

    간단 이미지 뷰어 (수정)

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnPrev, btnNext;
     MyPictureView myPicture;
     int curNum;
+    TextView textView;
     File[] imageFiles;
     String imageFname;
 
@@ -27,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         btnPrev = findViewById(R.id.btnPrev);
         btnNext = findViewById(R.id.btnNext);
         myPicture = findViewById(R.id.myPictureView1);
+        textView = findViewById(R.id.textView);
 
         imageFiles = new File(Environment.getExternalStorageDirectory()
                 .getAbsolutePath() + "/Pictures").listFiles();
@@ -39,10 +42,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(curNum <= 0){
+                    textView.setText((curNum + 1) +" / " + imageFiles.length);
+
                     Toast.makeText(getApplicationContext(),
                             "첫번째 그림입니다", Toast.LENGTH_SHORT).show();
                 }else{
                     curNum--;
+                    textView.setText((curNum + 1) + " / " + imageFiles.length);
+
                     imageFname = imageFiles[curNum].toString();
                     myPicture.imagePath = imageFname;
                     myPicture.invalidate(); // 화면 갱신
@@ -55,10 +62,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(curNum >= imageFiles.length - 1){
+                    textView.setText((curNum + 1) + " / " + imageFiles.length);
+
                     Toast.makeText(getApplicationContext(), "마지막 그림입니다",
                             Toast.LENGTH_SHORT).show();
                 }else{
                     curNum++;
+                    textView.setText((curNum + 1) + " / " + imageFiles.length);
+
                     imageFname = imageFiles[curNum].toString();
                     myPicture.imagePath = imageFname;
                     myPicture.invalidate();
