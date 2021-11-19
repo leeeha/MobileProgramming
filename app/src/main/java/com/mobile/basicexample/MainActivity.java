f8b1a4ba8e1d76d9ca8ce625c681516e9103bffc
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-19 11:24:18 +0900

    컨텍스트 메뉴 (XML 없이 자바 코드로만 메뉴에 아이템 추가하기)

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -37,37 +37,49 @@ public class MainActivity extends AppCompatActivity {
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
 
-        MenuInflater mInflater = getMenuInflater();
+        //MenuInflater mInflater = getMenuInflater();
 
         // 버튼1을 길게 누르면 컨텍스트 메뉴가 뜰 수 있도록
         if(v == button1){
             menu.setHeaderTitle("배경색 변경");
-            mInflater.inflate(R.menu.menu1, menu);
+
+            //mInflater.inflate(R.menu.menu1, menu);
+            menu.add(0, 1, 0, "배경색 (빨강)");
+            menu.add(0, 2, 0, "배경색 (초록)");
+            menu.add(0, 3, 0, "배경색 (파랑)");
         }
 
         // 버튼2를 길게 누르면 컨텍스트 메뉴가 뜰 수 있도록
         if(v == button2){
             menu.setHeaderTitle("버튼 변경");
-            mInflater.inflate(R.menu.menu2, menu);
+
+            //mInflater.inflate(R.menu.menu2, menu);
+            menu.add(0, 4, 0, "버튼 45도 회전");
+            menu.add(0, 5, 0, "버튼 2배 확대");
         }
     }
 
     @Override
     public boolean onContextItemSelected(@NonNull MenuItem item) {
         switch(item.getItemId()){
-            case R.id.itemRed:
+            //case R.id.itemRed:
+            case 1:
                 baseLayout.setBackgroundColor(Color.RED);
                 return true;
-            case R.id.itemGreen:
+            //case R.id.itemGreen:
+            case 2:
                 baseLayout.setBackgroundColor(Color.GREEN);
                 return true;
-            case R.id.itemBlue:
+            //case R.id.itemBlue:
+            case 3:
                 baseLayout.setBackgroundColor(Color.BLUE);
                 return true;
-            case R.id.subRotate:
+            //case R.id.subRotate:
+            case 4:
                 button2.setRotation(45);
                 return true;
-            case R.id.subSize:
+            //case R.id.subSize:
+            case 5:
                 button2.setScaleX(2);
                 return true;
         }
