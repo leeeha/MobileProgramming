176f0eb1c5a8e3e4d77d9ef70d520302fbd1ae58
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-21 00:12:39 +0900

    '어댑터'는 '데이터'를 '어댑터 뷰'에 보여주는 중간 객체 역할 - 데이터: 배열, 연결리스트, DB 등 - 어댑터 뷰: ListView, GridView, Gallery, Spinner 등 - 어댑터: ArrayAdapter, CursorAdapter, SimpleAdapter 등

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -1,41 +1,20 @@
 package com.mobile.basicexample;
 
-import android.app.SearchManager;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-/*  액티비티의 생명 주기
-
-    onCreate() 액티비티 초기화 작업
-
-    onStart() 사용자가 액티비티를 볼 수 있는 상태
-
-    onResume() 액티비티가 실제 사용자와 상호작용이 가능한 포그라운드에 위치한 상태
-
-    [액티비티 실행 중]
-
-    onPause() 사용자와 상호작용이 불가능한, 포커스를 잃은 상태 <-> onResume()
-
-    onStop() 액티비티가 사용자에게 더 이상 보이지 않는 상태 <-> onRestart() -> onStart()
-           cf) 새로 호출되는 액티비티의 스타일을 translucent(투명)으로 설정하면,
-               기존 액티비티도 눈에 보이기 때문에 onPause()까지만 호출됨.
-               하지만, 일반적으로 새로 뜬 화면 때문에 기존 화면이 보이지 않으면
-               onPause(), onStop()이 같이 호출됨.
-
-    onDestroy() 액티비티 또는 앱 프로세스 자체가 종료된 상태
-
-    cf) 다이얼로그는 새로운 액티비티를 띄우는 게 아니라, 단지 액티비티의 일부이기 때문에
-        생명주기 함수가 호출되지 않는다.
+/*
+    '어댑터'는 데이터 소스(배열, 연결리스트, DB 등)를
+    어댑터 뷰(ListView, GridView, Gallery, Spinner 등)에 보여주는
+    일종의 다리 역할을 하는 객체! (데이터와 어댑터 뷰 사이의 bridge)
+    ex) ArrayAdapter, CursorAdapter, SimpleAdapter 등
  */
 
 public class MainActivity extends AppCompatActivity {
@@ -44,92 +23,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("액티비티 생명주기");
+        setTitle("ListView 테스트");
 
-        android.util.Log.i("액티비티 테스트", "onCreate()");
+        // String 배열은 리스트 뷰에 보여줄 데이터
+        final String[] movie = {"히어로즈", "24시", "로스트", "스몰빌", "빅뱅이론",
+                "히어로즈", "24시", "로스트", "스몰빌", "빅뱅이론",
+                "히어로즈", "24시", "로스트", "스몰빌", "빅뱅이론"};
 
-        Button btnDial = findViewById(R.id.btnDial);
-        btnDial.setOnClickListener(new View.OnClickListener() {
+        // ListView는 어댑터 뷰
+        ListView list = findViewById(R.id.listView1);
+
+        // 어댑터는 데이터(String 배열)를 어댑터 뷰(리스트 뷰)에 보여주는 중간 객체
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+//                android.R.layout.simple_list_item_1, movie);
+
+        // 라디오 버튼
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+//                android.R.layout.simple_list_item_single_choice, movie);
+//        list.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        // 체크 박스
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+               android.R.layout.simple_list_item_multiple_choice, movie);
+        list.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+        // 어댑터 뷰에 어댑터 설정
+        list.setAdapter(adapter);
+
+        // 리스트 뷰의 항목을 클릭하면 토스트 메시지 띄우기
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                // 암시적 인텐트
-                Uri uri = Uri.parse("tel: 010-1234-5678");
-                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
-                startActivity(intent);
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), movie[i],
+                        Toast.LENGTH_SHORT).show();
             }
         });
 
-        Button btnNewActivity = findViewById(R.id.btnNewActivity);
-        btnNewActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 명시적 인텐트
-                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button btnFinish = findViewById(R.id.btnFinish);
-        btnFinish.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish(); // 액티비티 종료 (onDestroy)
-            }
-        });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        android.util.Log.i("액티비티 테스트", "onStart()");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        android.util.Log.i("액티비티 테스트", "onResume()");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        android.util.Log.i("액티비티 테스트", "onPause()");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        android.util.Log.i("액티비티 테스트", "onStop()");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        android.util.Log.i("액티비티 테스트", "onDestroy()");
     }
 }
-
-/* Logcat 확인 결과
-
-    [메인 액티비티 시작]
-    I/액티비티 테스트: onCreate()
-    I/액티비티 테스트: onStart()
-    I/액티비티 테스트: onResume()
-
-    [메인 액티비티 실행 중]
-
-    [다른 액티비티 요청]
-    I/액티비티테스트: onPause() // 투명한 액티비티일 경우 onPause()까지만 호출됨.
-    I/액티비티 테스트: onStop()
-
-    [메인 액티비티로 돌아왔을 때]
-    I/액티비티 테스트: onStart()
-    I/액티비티 테스트: onResume() // 투명한 액티비티일 경우 onResume()만 호출됨.
-
-    [메인 액티비티 종료]
-    I/액티비티 테스트: onPause()
-    I/액티비티 테스트: onStop()
-    I/액티비티 테스트: onDestroy()
-
- */
-
