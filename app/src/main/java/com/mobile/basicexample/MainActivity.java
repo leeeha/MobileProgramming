568c1e0f64ffdd41f5d1b241e8a5ed6a8a6e5c6d
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-21 02:59:34 +0900

    Spinner에서 선택한 항목에 따른 이미지 보여주기

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Gallery;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,16 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Gallery로 영화 포스터 보여주기");
-
-        Gallery gallery = findViewById(R.id.gallery1);
-        MyGalleryAdapter galAdapter = new MyGalleryAdapter(this);
-        gallery.setAdapter(galAdapter);
-
-    }
-
-    private class MyGalleryAdapter extends BaseAdapter {
-        Context context;
+        setTitle("Spinner (ComboBox) 예제");
 
         Integer[] posterID = {
                 R.drawable.mov11, R.drawable.mov12, R.drawable.mov13,
@@ -39,67 +33,33 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.mov20
         };
 
-        String[] movieTitle = {
+        final String[] movie = {
                 "여인의 향기", "쥬라기 공원", "포레스트 검프",
                 "사랑의 블랙홀", "혹성탈출", "아름다운 비행",
                 "내 이름은 칸", "해리포터", "마더",
                 "킹콩을 들다"
         };
 
-        public MyGalleryAdapter(Context c){
-            context = c;
-        }
+        Spinner spinner = findViewById(R.id.spinner1);
 
-        @Override
-        public int getCount() {
-            return posterID.length;
-        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, movie);
+        spinner.setAdapter(adapter);
 
-        @Override
-        public Object getItem(int i) {
-            return null;
-        }
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ImageView imageView = findViewById(R.id.ivPoster);
+                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                imageView.setImageResource(posterID[i]);
+            }
 
-        @Override
-        public long getItemId(int i) {
-            return posterID[i];
-        }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
-        @Override
-        public View getView(int position, View view, ViewGroup viewGroup) {
-            ImageView imageView = new ImageView(context);
+            }
+        });
 
-            // ViewGroup.LayoutParams(X) Gallery.LayoutParams (O)
-            imageView.setLayoutParams(new Gallery.LayoutParams(200, 300));
-            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-            imageView.setPadding(5, 5, 5, 5);
-            imageView.setImageResource(posterID[position]);
-
-            final int pos = position;
-            imageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    // 이미지 초기화
-                    ImageView ivPoster = findViewById(R.id.ivPoster);
-                    ivPoster.setScaleType(ImageView.ScaleType.FIT_CENTER);
-                    ivPoster.setImageResource(posterID[pos]);
-
-                    // toast.xml 레이아웃 파일을
-                    // 자바 코드 내의 toastView 객체에 인플레이트 시키기
-                    Toast toast = new Toast(getApplicationContext());
-                    View toastView = View.inflate(getApplicationContext(),
-                            R.layout.toast, null);
-
-                    // 인플레이트시킨 toastView의 텍스트 뷰 변경하고 setView로 지정
-                    TextView toastText = toastView.findViewById(R.id.movieTitle);
-                    toastText.setText(movieTitle[pos]);
-                    toast.setView(toastView);
-                    toast.show();
-                }
-            });
-
-            return imageView; // NullPointerException 주의!!!!
-        }
     }
 }
 
