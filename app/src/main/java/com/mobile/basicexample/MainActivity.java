283acb098323e6588094128b0e11d34dbdba36c4
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-20 19:27:56 +0900

    액티비티 간에 양방향으로 데이터 전달

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -15,50 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("명화 선호도 투표");
+        setTitle("메인 액티비티");
 
-        final int voteCount[] = new int[5];
-        for (int i = 0; i < 5; i++) {
-            voteCount[i] = 0;
-        }
-
-        ImageView image[] = new ImageView[5];
-        Integer imageId[] = { R.id.iv1, R.id.iv2, R.id.iv3, R.id.iv4, R.id.iv5 };
-        final String imgName[] = {
-                "독서하는 소녀", "꽃장식 모자 소녀", "부채를 든 소녀",
-                "이레느깡 단 베르앙", "잠 자는 소녀"
-        };
-
-        // 이미지 클릭할 때마다 투표 수 토스트 메시지로 보여주기
-        for (int i = 0; i < imageId.length; i++) {
-            final int index;
-            index = i;
-            image[index] = findViewById(imageId[index]);
-            image[index].setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    voteCount[index]++;
-                    Toast.makeText(getApplicationContext(),
-                            imgName[index] + ": 총 " + voteCount[index] + "표",
-                            Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-
-        Button btnFinish = findViewById(R.id.btnResult);
-        btnFinish.setOnClickListener(new View.OnClickListener() {
+        Button btnNewActivity = findViewById(R.id.btnNewActivity);
+        btnNewActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),
-                        ResultActivity.class);
-                intent.putExtra("VoteCount", voteCount);
-                intent.putExtra("ImageName", imgName);
-
-                startActivity(intent);
+                EditText edtNum1 = findViewById(R.id.edtNum1);
+                EditText edtNum2 = findViewById(R.id.edtNum2);
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                intent.putExtra("Num1", Integer.parseInt(edtNum1.getText().toString()));
+                intent.putExtra("Num2", Integer.parseInt(edtNum2.getText().toString()));
+                startActivityForResult(intent, 0);
             }
         });
-
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            int sum = data.getIntExtra("합", 0);
+            Toast.makeText(getApplicationContext(), "합: " + sum,
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
 }
 
--- app/src/main/java/com/mobile/basicexample/SecondActivity.java
+++ app/src/main/java/com/mobile/basicexample/SecondActivity.java
@@ -1,5 +1,6 @@
 package com.mobile.basicexample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,10 +14,18 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second);
 
+        Intent inIntent = getIntent();
+        final int sum = inIntent.getIntExtra("Num1", 0) +
+                inIntent.getIntExtra("Num2", 0);
+
         Button btnReturn = findViewById(R.id.btnReturn);
         btnReturn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent outIntent = new Intent(getApplicationContext(), MainActivity.class);
+                outIntent.putExtra("합", sum);
+                setResult(RESULT_OK, outIntent);
+
                 finish(); // 현재 액티비티 종료
             }
         });
