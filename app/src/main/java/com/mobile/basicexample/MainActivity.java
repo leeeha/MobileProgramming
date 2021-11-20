818f6dfc7961c1faaf20f3acea51ea719e998648
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-21 00:25:05 +0900

    리스트뷰의 항목 추가 및 삭제는 ArrayList<T>의 add, remove 메소드 사용하기

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -4,18 +4,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-/*
-    '어댑터'는 데이터 소스(배열, 연결리스트, DB 등)를
-    어댑터 뷰(ListView, GridView, Gallery, Spinner 등)에 보여주는
-    일종의 다리 역할을 하는 객체! (데이터와 어댑터 뷰 사이의 bridge)
-    ex) ArrayAdapter, CursorAdapter, SimpleAdapter 등
- */
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,39 +22,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("ListView 테스트");
 
-        // String 배열은 리스트 뷰에 보여줄 데이터
-        final String[] movie = {"히어로즈", "24시", "로스트", "스몰빌", "빅뱅이론",
-                "히어로즈", "24시", "로스트", "스몰빌", "빅뱅이론",
-                "히어로즈", "24시", "로스트", "스몰빌", "빅뱅이론"};
-
-        // ListView는 어댑터 뷰
+        // movieList는 데이터, ListView는 어댑터 뷰
+        final ArrayList<String> movieList = new ArrayList<>();
         ListView list = findViewById(R.id.listView1);
 
-        // 어댑터는 데이터(String 배열)를 어댑터 뷰(리스트 뷰)에 보여주는 중간 객체
-//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-//                android.R.layout.simple_list_item_1, movie);
-
-        // 라디오 버튼
-//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-//                android.R.layout.simple_list_item_single_choice, movie);
-//        list.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-
-        // 체크 박스
+        // 어댑터는 데이터(movieList)를 어댑터 뷰(ListView)에 보여주는 중간 객체
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-               android.R.layout.simple_list_item_multiple_choice, movie);
-        list.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+                android.R.layout.simple_list_item_1, movieList);
 
         // 어댑터 뷰에 어댑터 설정
         list.setAdapter(adapter);
 
-        // 리스트 뷰의 항목을 클릭하면 토스트 메시지 띄우기
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        final EditText edtItem = findViewById(R.id.edtItem);
+        Button btnAdd = findViewById(R.id.btnAdd);
+
+        // 항목 추가
+        btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getApplicationContext(), movie[i],
-                        Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                movieList.add(edtItem.getText().toString());
+                adapter.notifyDataSetChanged();
             }
         });
 
+        // 항목을 길게 누르면 리스트에서 삭제
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                movieList.remove(i);
+                adapter.notifyDataSetChanged();
+
+                return false;
+            }
+        });
     }
 }
