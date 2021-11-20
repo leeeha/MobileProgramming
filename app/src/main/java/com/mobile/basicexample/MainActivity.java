98032c0c5b215b3a7ea67ae7edcb28bedf60f6fe
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-20 22:11:00 +0900

    암시적 인텐트란? 약속된 액션을 지정하여 안드로이드에서 제공하는 기본 응용 프로그램을 실행하는 것 (전화 걸기, 웹 브라우저, 갤러리, 음악 듣기, 동영상 보기 등)

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -1,7 +1,10 @@
 package com.mobile.basicexample;
 
+import android.app.SearchManager;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,32 +19,73 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("메인 액티비티");
+        setTitle("암시적 인텐트 예제");
 
-        Button btnNewActivity = findViewById(R.id.btnNewActivity);
-        btnNewActivity.setOnClickListener(new View.OnClickListener() {
+        Button btnDial = findViewById(R.id.btnDial);
+        Button btnWeb = findViewById(R.id.btnWeb);
+        Button btnGoogle = findViewById(R.id.btnGoogle);
+        Button btnSearch = findViewById(R.id.btnSearch);
+        Button btnSms = findViewById(R.id.btnSms);
+        Button btnPhoto = findViewById(R.id.btnPhoto);
+
+        btnDial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText edtNum1 = findViewById(R.id.edtNum1);
-                EditText edtNum2 = findViewById(R.id.edtNum2);
-                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-                intent.putExtra("Num1", Integer.parseInt(edtNum1.getText().toString()));
-                intent.putExtra("Num2", Integer.parseInt(edtNum2.getText().toString()));
-                startActivityForResult(intent, 0);
+                Uri uri = Uri.parse("tel: 010-1234-5678");
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
             }
         });
+
+        btnWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("http://www.hanbit.co.kr");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 한빛아카데미 출판사 위치인 듯
+                Uri uri = Uri.parse("http://maps.google.com/maps?q=" +
+                        37.554264 + "," + 126.913598);
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                intent.putExtra(SearchManager.QUERY, "안드로이드");
+                startActivity(intent);
+            }
+        });
+
+        btnSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.putExtra("sms_body", "안녕하세요?");
+                intent.setData(Uri.parse("smsto: " + Uri.encode("010-1234-5678")));
+                startActivity(intent);
+            }
+        });
+
+        btnPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
+            }
+        });
+
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode,
-                                    @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode == RESULT_OK) {
-            int sum = data.getIntExtra("합", 0);
-            Toast.makeText(getApplicationContext(), "합: " + sum,
-                    Toast.LENGTH_SHORT).show();
-        }
-    }
 }
 
