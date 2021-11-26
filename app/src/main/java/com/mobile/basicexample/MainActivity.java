63022420dc0aa5a9207fd1d8e9e6273b10937a12
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-26 15:59:56 +0900

    리사이클러뷰의 구성 요소, 기억하자! - 데이터 리스트 - 아이템 뷰가 나열되는 형태를 결정하는 '레이아웃 매니저' - 데이터를 아이템 단위의 뷰로 만들어서 화면에 표시해주는 '어댑터' - 데이터를 리스트 형태로 표시해주는 컨테이너 '리사이클러뷰' - 어댑터를 통해 만들어진 각 아이템 뷰는 '뷰홀더' 객체에 저장되어 필요에 따라 재활용됨.

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -1,39 +1,85 @@
 package com.mobile.basicexample;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    static final String[] LIST_MENU = {"LIST1", "LIST2", "LIST3"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("리스트뷰 사용하기");
+        setTitle("리사이클러뷰 예제");
 
-        // 데이터를 리스트뷰에 보여주려면 어댑터가 필요함.
-        // 데이터가 배열이므로 ArrayAdapter 사용
-        ArrayAdapter adapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1, LIST_MENU);
+        // 리사이클러뷰에 표시할 데이터 리스트 생성
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            list.add(String.format("Text %d", i));
+        }
 
-        ListView listView = findViewById(R.id.listview1);
-        listView.setAdapter(adapter);
+        // 리사이클러뷰에 LinearLayoutManager 객체 지정
+        RecyclerView recyclerView = findViewById(R.id.recycler1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                String strText = (String) parent.getItemAtPosition(position);
+        // 리사이클러뷰에 SimpleTextAdapter 객체 지정
+        SimpleTextAdapter adapter = new SimpleTextAdapter(list);
+        recyclerView.setAdapter(adapter);
 
-                Toast.makeText(getApplicationContext(), strText, Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
+    public class SimpleTextAdapter extends RecyclerView.Adapter<SimpleTextAdapter.ViewHolder> {
+        private ArrayList<String> mData;
+
+        // 아이템 뷰를 저장하는 뷰홀더 클래스 정의
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            TextView textView1;
+
+            ViewHolder(View itemView){
+                super(itemView);
+
+                // 아이템뷰에 표시될 텍스트뷰에 대한 참조를 가지도록
+                textView1 = itemView.findViewById(R.id.text1);
+            }
+        }
+
+        // 생성자에서 데이터 리스트 객체를 전달받음.
+        SimpleTextAdapter(ArrayList<String> list){
+            mData = list;
+        }
+
+        @Override // 아이템 뷰를 저장하는 뷰홀더 객체를 생성하여 리턴
+        public SimpleTextAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            Context context = parent.getContext();
+            LayoutInflater inflater =
+                    (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = inflater.inflate(R.layout.recyclerview_item, parent, false);
+            SimpleTextAdapter.ViewHolder vh = new SimpleTextAdapter.ViewHolder(view);
+
+            return vh;
+        }
+
+        @Override // position에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            String text = mData.get(position);
+            holder.textView1.setText(text);
+        }
+
+        @Override // 전체 데이터 개수 리턴
+        public int getItemCount() {
+            return mData.size();
+        }
+    }
 }
+
