56e72ea73277626321ec2a86826963d933574430
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-30 08:16:56 +0900

    Canvas 위에 Paint 칠하기 - Canvas는 도화지, 그 재료는 Bitmap, Paint는 크레파스 - View 클래스 상속 받아서 onDraw 함수 오버라이딩 하기 - onDraw 함수의 인자로 전달 받은 canvas 위에 Paint 칠하기 (선, 직사각형, 원, 텍스트 등)

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -1,84 +1,83 @@
 package com.mobile.basicexample;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setTitle("리사이클러뷰 예제");
-
-        // 리사이클러뷰에 표시할 데이터 리스트 생성
-        ArrayList<String> list = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            list.add(String.format("Text %d", i));
-        }
-
-        // 리사이클러뷰에 LinearLayoutManager 객체 지정
-        RecyclerView recyclerView = findViewById(R.id.recycler1);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        // 리사이클러뷰에 SimpleTextAdapter 객체 지정
-        SimpleTextAdapter adapter = new SimpleTextAdapter(list);
-        recyclerView.setAdapter(adapter);
-
+        setContentView(new MyGraphicView(this));
     }
 
-    public class SimpleTextAdapter extends RecyclerView.Adapter<SimpleTextAdapter.ViewHolder> {
-        private ArrayList<String> mData;
-
-        // 아이템 뷰를 저장하는 뷰홀더 클래스 정의
-        public class ViewHolder extends RecyclerView.ViewHolder {
-            TextView textView1;
-
-            ViewHolder(View itemView){
-                super(itemView);
-
-                // 아이템뷰에 표시될 텍스트뷰에 대한 참조를 가지도록
-                textView1 = itemView.findViewById(R.id.text1);
-            }
+    private static class MyGraphicView extends View {
+        public MyGraphicView(Context context){
+            super(context);
         }
 
-        // 생성자에서 데이터 리스트 객체를 전달받음.
-        SimpleTextAdapter(ArrayList<String> list){
-            mData = list;
-        }
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
 
-        @Override // 아이템 뷰를 저장하는 뷰홀더 객체를 생성하여 리턴
-        public SimpleTextAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            Context context = parent.getContext();
-            LayoutInflater inflater =
-                    (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            // Paint 관련 메소드: https://jwandroid.tistory.com/182
+            Paint paint = new Paint();
 
-            View view = inflater.inflate(R.layout.recyclerview_item, parent, false);
-            SimpleTextAdapter.ViewHolder vh = new SimpleTextAdapter.ViewHolder(view);
+            // 직선 그리기
+            paint.setAntiAlias(true); // Paint의 경계면을 부드럽게 처리할지 설정
+            paint.setColor(Color.GREEN);
+            canvas.drawLine(10, 10, 300, 10, paint);
 
-            return vh;
-        }
+            paint.setColor(Color.BLUE);
+            paint.setStrokeWidth(5f);
+            canvas.drawLine(10, 30, 300, 30, paint);
 
-        @Override // position에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            String text = mData.get(position);
-            holder.textView1.setText(text);
-        }
+            // 직사각형 그리기: https://black-jin0427.tistory.com/144
+            paint.setColor(Color.RED);
+            paint.setStrokeWidth(0f);
+            paint.setStyle(Paint.Style.FILL);
+            Rect rect1 = new Rect(10, 50, 10 + 100, 50 +100);
+            canvas.drawRect(rect1, paint);
 
-        @Override // 전체 데이터 개수 리턴
-        public int getItemCount() {
-            return mData.size();
+            paint.setStyle(Paint.Style.STROKE);
+            Rect rect2 = new Rect(130, 50, 130 + 100, 50 + 100);
+            canvas.drawRect(rect2, paint);
+
+            // 모서리가 둥근 직사각형
+            RectF rect3 = new RectF(250, 50, 250 + 100, 50 + 100);
+            canvas.drawRoundRect(rect3, 20, 20, paint);
+
+            // 원 그리기, drawArc()로는 부채꼴을 그릴 수 있음.
+            canvas.drawCircle(60, 220, 50, paint);
+
+            // 좌표에 따른 경로 그리기
+            paint.setStrokeWidth(5f);
+            Path path1 = new Path();
+            path1.moveTo(10, 290);
+            path1.lineTo(10 + 50, 290 + 50);
+            path1.lineTo(10 + 100, 290);
+            path1.lineTo(10 + 150, 290 + 50);
+            path1.lineTo(10 + 200, 290);
+            canvas.drawPath(path1, paint);
+
+            // 텍스트 그리기
+            paint.setStrokeWidth(0f);
+            paint.setTextSize(30f);
+            paint.setColor(Color.BLACK);
+            canvas.drawText("안드로이드", 10, 390, paint);
         }
     }
 }
