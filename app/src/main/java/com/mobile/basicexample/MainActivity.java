a141b3154efab351c6ef5bfbacf4a7664b106a53
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-30 08:57:51 +0900

    View의 터치 이벤트 처리 : 옵션 메뉴에서 그릴 도형을 선택한 뒤, 손가락으로 뷰를 터치했다가 떼면, 터치의 시작점과 끝점의 좌표를 알 수 있다. 그 좌표를 바탕으로 도형을 그린다. (선, 원, 사각형 등)

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -10,74 +10,109 @@ import android.graphics.RectF;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    final static int LINE = 1, CIRCLE = 2, RECT = 3;
+    static int curShape = LINE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new MyGraphicView(this));
+        setTitle("간단 그림판");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        menu.add(0, 1, 0, "선 그리기");
+        menu.add(0, 2, 0, "원 그리기");
+        menu.add(0, 3, 0, "사각형 그리기");
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case 1:
+                curShape = LINE;
+                return true;
+            case 2:
+                curShape = CIRCLE;
+                return true;
+            case 3:
+                curShape = RECT;
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private static class MyGraphicView extends View {
+        int startX = -1, startY = -1, stopX = -1, stopY = -1;
+
         public MyGraphicView(Context context){
             super(context);
+        }
+
+        // 터치한 지점의 좌표 값에 따라서 도형 그리기
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            switch(event.getAction()){
+                // 손가락으로 화면을 누르기 시작할 때
+                case MotionEvent.ACTION_DOWN:
+                    startX = (int) event.getX();
+                    startY = (int) event.getY();
+                    break;
+                // 터치 후 손가락을 움직일 때
+                case MotionEvent.ACTION_MOVE:
+                    break;
+                // 손가락을 화면에서 뗄 때
+                case MotionEvent.ACTION_UP:
+                    stopX = (int) event.getX();
+                    stopY = (int) event.getY();
+                    this.invalidate();
+                    break;
+                // 터치가 취소될 때
+                case MotionEvent.ACTION_CANCEL:
+                    break;
+            }
+            return true;
         }
 
         @Override
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
 
-            // Paint 관련 메소드: https://jwandroid.tistory.com/182
             Paint paint = new Paint();
-
-            // 직선 그리기
-            paint.setAntiAlias(true); // Paint의 경계면을 부드럽게 처리할지 설정
-            paint.setColor(Color.GREEN);
-            canvas.drawLine(10, 10, 300, 10, paint);
-
-            paint.setColor(Color.BLUE);
+            paint.setAntiAlias(true);
             paint.setStrokeWidth(5f);
-            canvas.drawLine(10, 30, 300, 30, paint);
-
-            // 직사각형 그리기: https://black-jin0427.tistory.com/144
-            paint.setColor(Color.RED);
-            paint.setStrokeWidth(0f);
-            paint.setStyle(Paint.Style.FILL);
-            Rect rect1 = new Rect(10, 50, 10 + 100, 50 +100);
-            canvas.drawRect(rect1, paint);
-
             paint.setStyle(Paint.Style.STROKE);
-            Rect rect2 = new Rect(130, 50, 130 + 100, 50 + 100);
-            canvas.drawRect(rect2, paint);
+            paint.setColor(Color.RED);
 
-            // 모서리가 둥근 직사각형
-            RectF rect3 = new RectF(250, 50, 250 + 100, 50 + 100);
-            canvas.drawRoundRect(rect3, 20, 20, paint);
-
-            // 원 그리기, drawArc()로는 부채꼴을 그릴 수 있음.
-            canvas.drawCircle(60, 220, 50, paint);
-
-            // 좌표에 따른 경로 그리기
-            paint.setStrokeWidth(5f);
-            Path path1 = new Path();
-            path1.moveTo(10, 290);
-            path1.lineTo(10 + 50, 290 + 50);
-            path1.lineTo(10 + 100, 290);
-            path1.lineTo(10 + 150, 290 + 50);
-            path1.lineTo(10 + 200, 290);
-            canvas.drawPath(path1, paint);
-
-            // 텍스트 그리기
-            paint.setStrokeWidth(0f);
-            paint.setTextSize(30f);
-            paint.setColor(Color.BLACK);
-            canvas.drawText("안드로이드", 10, 390, paint);
+            switch(curShape){
+                case LINE:
+                    canvas.drawLine(startX, startY, stopX, stopY, paint);
+                    break;
+                case CIRCLE:
+                    int radius = (int) Math.sqrt(Math.pow(stopX - startX, 2) +
+                            Math.pow(stopY - startY, 2));
+                    canvas.drawCircle(startX, startY, radius, paint);
+                    break;
+                case RECT:
+                    canvas.drawRect(startX, startY, stopX, stopY, paint);
+            }
         }
     }
 }
