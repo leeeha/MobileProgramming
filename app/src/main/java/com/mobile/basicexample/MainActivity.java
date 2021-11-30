b21b413fb15d841533dd6674e5f424974f2f3886
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-30 09:20:16 +0900

    비트맵 이미지 다루기 기본

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -1,118 +1,63 @@
 package com.mobile.basicexample;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Path;
-import android.graphics.Rect;
-import android.graphics.RectF;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    final static int LINE = 1, CIRCLE = 2, RECT = 3;
-    static int curShape = LINE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new MyGraphicView(this));
-        setTitle("간단 그림판");
+        setTitle("비트맵 다루기");
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-
-        menu.add(0, 1, 0, "선 그리기");
-        menu.add(0, 2, 0, "원 그리기");
-        menu.add(0, 3, 0, "사각형 그리기");
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch(item.getItemId()){
-            case 1:
-                curShape = LINE;
-                return true;
-            case 2:
-                curShape = CIRCLE;
-                return true;
-            case 3:
-                curShape = RECT;
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     private static class MyGraphicView extends View {
-        int startX = -1, startY = -1, stopX = -1, stopY = -1;
-
         public MyGraphicView(Context context){
             super(context);
-        }
-
-        // 터치한 지점의 좌표 값에 따라서 도형 그리기
-        @Override
-        public boolean onTouchEvent(MotionEvent event) {
-            switch(event.getAction()){
-                // 손가락으로 화면을 누르기 시작할 때
-                case MotionEvent.ACTION_DOWN:
-                    startX = (int) event.getX();
-                    startY = (int) event.getY();
-                    break;
-                // 터치 후 손가락을 움직일 때
-                case MotionEvent.ACTION_MOVE:
-                    break;
-                // 손가락을 화면에서 뗄 때
-                case MotionEvent.ACTION_UP:
-                    stopX = (int) event.getX();
-                    stopY = (int) event.getY();
-                    this.invalidate();
-                    break;
-                // 터치가 취소될 때
-                case MotionEvent.ACTION_CANCEL:
-                    break;
-            }
-            return true;
         }
 
         @Override
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
 
-            Paint paint = new Paint();
-            paint.setAntiAlias(true);
-            paint.setStrokeWidth(5f);
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setColor(Color.RED);
+            // /res/drawable 폴더에서 이미지 파일 가져오는 경우
+            Bitmap picture = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.renoir01);
+            // sd 카드에서 이미지 파일 가져오는 경우
+            //Bitmap picture = BitmapFactory.decodeFile("파일경로 및 파일");
 
-            switch(curShape){
-                case LINE:
-                    canvas.drawLine(startX, startY, stopX, stopY, paint);
-                    break;
-                case CIRCLE:
-                    int radius = (int) Math.sqrt(Math.pow(stopX - startX, 2) +
-                            Math.pow(stopY - startY, 2));
-                    canvas.drawCircle(startX, startY, radius, paint);
-                    break;
-                case RECT:
-                    canvas.drawRect(startX, startY, stopX, stopY, paint);
-            }
+            int cenX = this.getWidth() / 2;
+            int cenY = this.getHeight() / 2;
+            int picX = (this.getWidth() - picture.getWidth()) / 2;
+            int picY = (this.getHeight() - picture.getHeight()) / 2;
+
+            // 화면 중앙에 이미지 배치
+            //canvas.drawBitmap(picture, picX, picY, null);
+
+            canvas.rotate(45, cenX, cenY); // 회전
+            canvas.drawBitmap(picture, picX, picY, null);
+
+            canvas.translate(-150, 200); // 좌표 이동
+            canvas.drawBitmap(picture, picX, picY, null);
+
+            canvas.scale(2f, 2f, cenX, cenY); // 크기 확대, 축소
+            canvas.drawBitmap(picture, picX, picY, null);
+
+            canvas.skew(0.3f, 0.3f); // 기울기
+            canvas.drawBitmap(picture, picX, picY, null);
+
+            picture.recycle();
         }
     }
 }
