36be03c674ab0b69e8ed772c41ce889724e94618
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-30 10:05:48 +0900

    컬러 매트릭스

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -6,6 +6,8 @@ import android.graphics.BitmapFactory;
 import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.EmbossMaskFilter;
 import android.graphics.Paint;
 import android.os.Bundle;
@@ -14,15 +16,15 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+// 색 변경을 위해 안드로이드에서 제공하는 행렬(Matrix)
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new MyGraphicView(this));
-        setTitle("엠보싱 효과");
+        setTitle("ColorMatrix 예제");
     }
-
 
     private static class MyGraphicView extends View {
         public MyGraphicView(Context context){
@@ -33,17 +35,24 @@ public class MainActivity extends AppCompatActivity {
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
 
-            int cenX = this.getWidth() / 2;
-            int cenY = this.getHeight() / 2;
+            Bitmap picture = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.renoir01);
+
+            int picX = (this.getWidth() - picture.getWidth()) / 2;
+            int picY = (this.getHeight() - picture.getHeight()) / 2;
 
             Paint paint = new Paint();
-            paint.setColor(Color.GRAY);
 
-            // EmbossMaskFilter(빛의 xyz 방향, 빛의 밝기, 반사 계수, 블러링 크기);
-            EmbossMaskFilter eMask =
-                    new EmbossMaskFilter(new float[] {3, 10, 3}, 0.5f, 5, 10);
-            paint.setMaskFilter(eMask);
-            canvas.drawCircle(cenX, cenY, 150, paint);
+            // 색상 대비를 2배로 변경
+            float[] array = { 2, 0, 0, 0, 0, // red vector
+                              0, 2, 0, 0, 0, // green vector
+                              0, 0, 2, 0, 0, // blue vector
+                              0, 0, 0, 1, 0 }; // alpha vector (불투명도)
+            ColorMatrix cm = new ColorMatrix(array);
+            paint.setColorFilter(new ColorMatrixColorFilter(cm));
+
+            canvas.drawBitmap(picture, picX, picY, paint);
+            picture.recycle();
         }
     }
 }
