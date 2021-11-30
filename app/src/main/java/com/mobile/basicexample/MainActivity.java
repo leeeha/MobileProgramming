3221f4dddeeebca1a1ce9cc5b5b65d80ea137183
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-30 09:22:25 +0900

    블러링 효과

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -34,29 +34,17 @@ public class MainActivity extends AppCompatActivity {
             // /res/drawable 폴더에서 이미지 파일 가져오는 경우
             Bitmap picture = BitmapFactory.decodeResource(getResources(),
                     R.drawable.renoir01);
-            // sd 카드에서 이미지 파일 가져오는 경우
-            //Bitmap picture = BitmapFactory.decodeFile("파일경로 및 파일");
 
-            int cenX = this.getWidth() / 2;
-            int cenY = this.getHeight() / 2;
             int picX = (this.getWidth() - picture.getWidth()) / 2;
             int picY = (this.getHeight() - picture.getHeight()) / 2;
 
-            // 화면 중앙에 이미지 배치
-            //canvas.drawBitmap(picture, picX, picY, null);
+            Paint paint = new Paint();
 
-            canvas.rotate(45, cenX, cenY); // 회전
-            canvas.drawBitmap(picture, picX, picY, null);
+            // 블러 마스크 필터: NORMAL, INNER, OUTER, SOLID
+            BlurMaskFilter bMask = new BlurMaskFilter(30, BlurMaskFilter.Blur.NORMAL);
+            paint.setMaskFilter(bMask);
 
-            canvas.translate(-150, 200); // 좌표 이동
-            canvas.drawBitmap(picture, picX, picY, null);
-
-            canvas.scale(2f, 2f, cenX, cenY); // 크기 확대, 축소
-            canvas.drawBitmap(picture, picX, picY, null);
-
-            canvas.skew(0.3f, 0.3f); // 기울기
-            canvas.drawBitmap(picture, picX, picY, null);
-
+            canvas.drawBitmap(picture, picX, picY, paint);
             picture.recycle();
         }
     }
