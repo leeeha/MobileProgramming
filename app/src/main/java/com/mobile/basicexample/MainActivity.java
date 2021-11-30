abddb96dbfc2e73cbe1cfa5ed35ce790f0436b5c
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-30 10:37:21 +0900

    이미지 확대/축소, 회전, 밝기, 채도 조절

--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -13,20 +13,93 @@ import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-// 색 변경을 위해 안드로이드에서 제공하는 행렬(Matrix)
 public class MainActivity extends AppCompatActivity {
+    ImageButton ibZoomIn, ibZoomOut, ibRotate, ibBright, ibDark, ibGray;
+    MyGraphicView graphicView;
+    static float scaleX = 1, scaleY = 1;
+    static float angle = 0;
+    static float color = 1;
+    static float satur = 1; // 채도 saturation
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new MyGraphicView(this));
-        setTitle("ColorMatrix 예제");
+        setContentView(R.layout.activity_main);
+        setTitle("미니 포토샵");
+
+        LinearLayout pictureLayout = findViewById(R.id.pictureLayout);
+        graphicView = new MyGraphicView(this);
+        pictureLayout.addView(graphicView);
+
+        clickIcons();
+
+    }
+
+    private void clickIcons(){
+        ibZoomIn = findViewById(R.id.ibZoomIn);
+        ibZoomIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scaleX += 0.2f;
+                scaleY += 0.2f;
+                graphicView.invalidate(); // onDraw 함수 다시 호출
+            }
+        });
+
+        ibZoomOut = findViewById(R.id.ibZoomOut);
+        ibZoomOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scaleX -= 0.2f;
+                scaleY -= 0.2f;
+                graphicView.invalidate();
+            }
+        });
+
+        ibRotate = findViewById(R.id.ibRotate);
+        ibRotate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                angle -= 20; // 반시계 방향으로 회전
+                graphicView.invalidate();
+            }
+        });
+
+        ibBright = findViewById(R.id.ibBright);
+        ibBright.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                color += 0.2f;
+                graphicView.invalidate();
+            }
+        });
+
+        ibDark = findViewById(R.id.ibDark);
+        ibDark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                color -= 0.2f;
+                graphicView.invalidate();
+            }
+        });
+
+        ibGray = findViewById(R.id.ibGray);
+        ibGray.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                satur = (satur == 0) ? 1 : 0; // 채도 반전
+                graphicView.invalidate();
+            }
+        });
     }
 
     private static class MyGraphicView extends View {
+
         public MyGraphicView(Context context){
             super(context);
         }
@@ -35,23 +108,31 @@ public class MainActivity extends AppCompatActivity {
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
 
-            Bitmap picture = BitmapFactory.decodeResource(getResources(),
-                    R.drawable.renoir01);
-
-            int picX = (this.getWidth() - picture.getWidth()) / 2;
-            int picY = (this.getHeight() - picture.getHeight()) / 2;
+            int cenX = this.getWidth() / 2;
+            int cenY = this.getHeight() / 2;
+            canvas.scale(scaleX, scaleY, cenX, cenY); // 크기 설정
+            canvas.rotate(angle, cenX, cenY); // 각도 설정
 
             Paint paint = new Paint();
 
-            // 색상 대비를 2배로 변경
-            float[] array = { 2, 0, 0, 0, 0, // red vector
-                              0, 2, 0, 0, 0, // green vector
-                              0, 0, 2, 0, 0, // blue vector
-                              0, 0, 0, 1, 0 }; // alpha vector (불투명도)
+            // 컬러 매트릭스로 이미지의 밝기, 채도 설정
+            float[] array = {
+                    color, 0, 0, 0, 0, // red vector
+                    0, color, 0, 0, 0, // green vector
+                    0, 0, color, 0, 0, // blue vector
+                    0, 0, 0, 1, 0, // alpha vector
+            };
             ColorMatrix cm = new ColorMatrix(array);
+            if(satur == 0){
+                cm.setSaturation(satur); // 채도 설정
+            }
             paint.setColorFilter(new ColorMatrixColorFilter(cm));
 
+            Bitmap picture = BitmapFactory.decodeResource(getResources(), R.drawable.renoir01);
+            int picX = (this.getWidth() - picture.getWidth()) / 2;
+            int picY = (this.getHeight() - picture.getHeight()) / 2;
             canvas.drawBitmap(picture, picX, picY, paint);
+
             picture.recycle();
         }
     }
