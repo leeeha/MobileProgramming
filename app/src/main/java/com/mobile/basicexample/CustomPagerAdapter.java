98fe6aa47d293c02e2348685f77f483b7c6e8895
leeeha <jxlhe46@seoultech.ac.kr>
2021-12-03 15:34:04 +0900

    뷰페이저 안에 리사이클러뷰 생성하기

--- app/src/main/java/com/mobile/basicexample/CustomPagerAdapter.java
+++ app/src/main/java/com/mobile/basicexample/CustomPagerAdapter.java
@@ -7,7 +7,11 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.ArrayList;
 
 public class CustomPagerAdapter extends PagerAdapter {
     // LayoutInflater 서비스 사용을 위한 Context 참조
@@ -24,12 +28,26 @@ public class CustomPagerAdapter extends PagerAdapter {
 
         if(context != null){
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            pageView = inflater.inflate(R.layout.viewpager_item, container, false);
+            pageView = inflater.inflate(R.layout.page, container, false);
 
-            TextView textView = pageView.findViewById(R.id.textView);
-            textView.setText("TEXT" + position);
+            // 리사이클러뷰를 메인 액티비티 말고, 뷰페이저 안에서 띄우자!!!
+            // 페이지뷰 하나 생성할 때마다 새로운 리사이클러뷰가 뜨도록
+            ArrayList<String> list = new ArrayList<>();
+            for (int i = 0; i < 10; i++) {
+                list.add(String.format("TEXT %d", i));
+            }
+
+            // 리사이클러뷰의 레이아웃 매니저 지정
+            RecyclerView recyclerView = pageView.findViewById(R.id.recyclerview_main_list);
+            recyclerView.setLayoutManager(new LinearLayoutManager(context.getApplicationContext()));
+
+            // 리사이클러뷰의 어댑터 지정
+            RecyclerAdapter adapter = new RecyclerAdapter(list);
+            recyclerView.setAdapter(adapter);
+
         }
 
+        // 뷰페이저인 container에 리사이클러뷰를 담고 있는 pageView 추가하기
         container.addView(pageView);
 
         return pageView;
@@ -43,11 +61,52 @@ public class CustomPagerAdapter extends PagerAdapter {
 
     @Override
     public int getCount() {
-        return 10; // 전체 페이지수는 10개로 고정
+        return 5; // 전체 페이지수는 10개로 고정
     }
 
-    @Override
+    @Override // 페이지뷰가 특정 키 객체(key object)와 연관되는지
     public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
         return (view == (View)object);
+    }
+
+    // 이너 클래스로 리사이클러뷰 어댑터 정의...
+    public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.CustomViewHolder>{
+        private ArrayList<String> data;
+
+        // 리사이클러뷰의 아이템 하나를 저장하는 뷰홀더 클래스
+        public class CustomViewHolder extends RecyclerView.ViewHolder{
+            TextView textView;
+
+            CustomViewHolder(View itemView){
+                super(itemView);
+
+                // 리사이클러뷰 안에 있는 항목들 참조
+                textView = itemView.findViewById(R.id.textView);
+            }
+        }
+
+        public RecyclerAdapter(ArrayList<String> list){
+            this.data = list;
+        }
+
+        @NonNull
+        @Override // 아이템 뷰를 위한 뷰홀더 객체 생성하여 리턴
+        public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.recyclerview_item, parent, false);
+
+            return new CustomViewHolder(view);
+        }
+
+        @Override // position에 해당하는 데이터를 뷰홀더의 아이템 뷰에 표시
+        public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
+            String text = data.get(position);
+            holder.textView.setText(text);
+        }
+
+        @Override // 리사이클러뷰에 담겨 있는 전체 아이템 개수 리턴
+        public int getItemCount() {
+            return data.size();
+        }
     }
 }
--- app/src/main/java/com/mobile/basicexample/MainActivity.java
+++ app/src/main/java/com/mobile/basicexample/MainActivity.java
@@ -1,11 +1,21 @@
 package com.mobile.basicexample;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
     private ViewPager viewPager;
     private CustomPagerAdapter pagerAdapter;
 
@@ -13,12 +23,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("뷰페이저 기본 사용법");
+        setTitle("뷰페이저 안에 리사이클러뷰 생성하기");
 
         viewPager = findViewById(R.id.viewPager);
         pagerAdapter = new CustomPagerAdapter(this);
         viewPager.setAdapter(pagerAdapter);
-
     }
 }
 
